520ad473feef27866b129aad68587babcdb60c64
Kavindu Maleesha <152074659+Kaviii18@users.noreply.github.com>
2025-05-05 11:13:48 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+// config.php
+
+// Database configuration
+$host = "localhost";
+$username = "root"; 
+$password = ""; 
+$database = "gym_database1"; 
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
